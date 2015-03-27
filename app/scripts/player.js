17611fd476f9af63640b0e2af6457bff9392309b
Snaebjorn Eyjolfsson <snabbie1@gmail.com>
2015-03-27 10:44:44 +0000

    added basic jumping functionality, and wings animation

--- app/scripts/player.js
+++ app/scripts/player.js
@@ -6,7 +6,7 @@ window.Player = (function() {
 	// All these constants are in em's, multiply by 10 pixels
 	// for 1024x576px canvas.
 	var SPEED = 30; // * 10 pixels per second
-	var WIDTH = 5;
+	// var WIDTH = 5;
 	var HEIGHT = 5;
 	var INITIAL_POSITION_X = 30;
 	var INITIAL_POSITION_Y = 25;
@@ -15,6 +15,7 @@ window.Player = (function() {
 		this.el = el;
 		this.game = game;
 		this.pos = { x: 0, y: 0 };
+		this.playing = false;
 	};
 
 	/**
@@ -23,10 +24,11 @@ window.Player = (function() {
 	Player.prototype.reset = function() {
 		this.pos.x = INITIAL_POSITION_X;
 		this.pos.y = INITIAL_POSITION_Y;
+		this.playing = false;
 	};
 
 	Player.prototype.onFrame = function(delta) {
-		if (Controls.keys.right) {
+		/* if (Controls.keys.right) {
 			this.pos.x += delta * SPEED;
 		}
 		if (Controls.keys.left) {
@@ -37,6 +39,18 @@ window.Player = (function() {
 		}
 		if (Controls.keys.up) {
 			this.pos.y -= delta * SPEED;
+		} */
+
+		if (Controls.keys.space) {
+			if (this.playing) {
+				this.pos.y -= delta * SPEED * 4;
+			} else {
+				this.playing = true;
+			}
+		} else {
+			if (this.playing) {
+				this.pos.y += delta * SPEED * 1;
+			}
 		}
 
 		this.checkCollisionWithBounds();
@@ -46,10 +60,7 @@ window.Player = (function() {
 	};
 
 	Player.prototype.checkCollisionWithBounds = function() {
-		if (this.pos.x < 0 ||
-			this.pos.x + WIDTH > this.game.WORLD_WIDTH ||
-			this.pos.y < 0 ||
-			this.pos.y + HEIGHT > this.game.WORLD_HEIGHT) {
+		if (this.pos.y + HEIGHT > this.game.WORLD_HEIGHT) {
 			return this.game.gameover();
 		}
 	};
